75f9d0c10478d73d637b79bd9dcdbfd27a2a44a8
patorma <pocontreras@ing.ucsc.cl>
2024-07-24 23:30:31 -0400

    Configuracion inicial para la conexion a la bd

---
+++ src/main/java/com/patricio/contreras/AdministracionClientesApplication.java
@@ -0,0 +1,13 @@
+package com.patricio.contreras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdministracionClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdministracionClientesApplication.class, args);
+	}
+
+}
