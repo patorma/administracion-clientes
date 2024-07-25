3c3b556a1346b16ccd447945977aa9d3be2db1e9
patorma <pocontreras@ing.ucsc.cl>
2024-07-25 01:57:01 -0400

    se creó las interface dao de ciudad y region

---
+++ src/main/java/com/patricio/contreras/dao/ICiudadDao.java
@@ -0,0 +1,7 @@
+package com.patricio.contreras.dao;
+
+import com.patricio.contreras.entity.Ciudad;
+
+public interface ICiudadDao extends IGenericDao<Ciudad, Long> {
+
+}
---
+++ src/main/java/com/patricio/contreras/dao/IRegionDao.java
@@ -0,0 +1,7 @@
+package com.patricio.contreras.dao;
+
+import com.patricio.contreras.entity.Region;
+
+public interface IRegionDao extends IGenericDao<Region, Long>{
+
+}
