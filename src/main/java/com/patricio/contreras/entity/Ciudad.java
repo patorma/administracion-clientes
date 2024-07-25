135c3bf539f81c9e6bfb38502e0be53b7783ff7d
patorma <pocontreras@ing.ucsc.cl>
2024-07-25 01:32:59 -0400

    se agregó la relación de la ciudad con la región

--- src/main/java/com/patricio/contreras/entity/Ciudad.java
+++ src/main/java/com/patricio/contreras/entity/Ciudad.java
@@ -2,13 +2,19 @@ package com.patricio.contreras.entity;
 
 import java.io.Serializable;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -32,6 +38,11 @@ public class Ciudad implements Serializable {
 	@Size(min = 3,max = 20)
 	private String nombreCiudad;
 	
+	@NotNull(message = "no puede estar vacio")
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "region_id")
+	@JsonIgnoreProperties({"hibernateLazyInitializer","hadler"})
+	private Region region;
 	
 	private static final long serialVersionUID = 1L;
 
